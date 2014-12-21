b2107d233edde47222518790f52db6cbee095dc8
Edwin Zhang <ehzhang@mit.edu>
2014-12-21 04:10:10 -0500

    fresh new site yo

--- js/site.js
+++ js/site.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
-  var $splash = $('#splash');
+  $('#header')
+      .find('.content')
+      .fadeIn(2000);
 
-  $splash
-    .find('.title')
-    .transition('fade down in', 2000);
-  $splash
-    .find('.body')
-    .transition('fade up in', 2000);
-
-  $('a').click(function(){
+  $('a.down').click(function(){
     $('html, body').animate({
       scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 500);
